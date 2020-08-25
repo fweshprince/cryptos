7466345f0c0c339f01e5b07d3c773025bedab19e
fweshprince <j.inglewood007@gmail.com>
2020-08-25 16:51:50 +0200

    fixed views

--- routers/index.js
+++ routers/index.js
@@ -14,13 +14,14 @@ const {
   withdraw,
 } = require("../controllers/index");
 
-router.route("/about").get(about);
+router.route("/about-us").get(about);
 router.route("/activePlans").get(activePlans);
 router.route("/confirmation").get(confirmation);
 router.route("/contact").get(contact);
 router.route("/dashboard").get(dashboard);
 router.route("/faq").get(faq);
 router.route("/").get(index);
+router.route("/index").get(index);
 router.route("/invest").get(invest);
 router.route("/login").get(signin);
 router.route("/rules").get(rules);
