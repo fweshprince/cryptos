42900be73ddf6686c07f93d4cb5bf5c62702750c
fweshprince <j.inglewood007@gmail.com>
2020-08-25 17:04:52 +0200

    fixed signup

--- controllers/index.js
+++ controllers/index.js
@@ -1,60 +1,60 @@
 // @desc Render about page
 // @access public
 exports.about = (req, res, next) => {
-    res.render('about-us')
-}
+  res.render("about-us");
+};
 // @desc Render user actiive plans page
 // @access private
 exports.activePlans = (req, res, next) => {
-    res.render('activeplans')
-}
+  res.render("activeplans");
+};
 // @desc Render payment confirmation page
 // @access private
 exports.confirmation = (req, res, next) => {
-    res.render('confirmation')
-}
+  res.render("confirmation");
+};
 // @desc Render user dashboard page
 // @access private
 exports.dashboard = (req, res, next) => {
-    res.render('dashboard')
-}
+  res.render("dashboard");
+};
 // @desc Render invest page
 // @access private
 exports.invest = (req, res, next) => {
-    res.render('invest')
-}
+  res.render("invest");
+};
 // @desc Render make withdrawal page
 // @access private
 exports.withdraw = (req, res, next) => {
-    res.render('withdraw')
-}
+  res.render("withdraw");
+};
 // @desc Render contact us page
 // @access public
 exports.contact = (req, res, next) => {
-    res.render('contact')
-}
+  res.render("contact");
+};
 // @desc Render faq page
 // @access public
 exports.faq = (req, res, next) => {
-    res.render('faq')
-}
+  res.render("faq");
+};
 // @desc Render home page
 // @access public
 exports.index = (req, res, next) => {
-    res.render('index')
-}
+  res.render("index");
+};
 // @desc Render signin page
 // @access public
 exports.signin = (req, res, next) => {
-    res.render('login')
-}
+  res.render("login");
+};
 // @desc Render rules page
 // @access public
 exports.rules = (req, res, next) => {
-    res.render('rules')
-}
+  res.render("rules");
+};
 // @desc Render signup page
 // @access public
 exports.signup = (req, res, next) => {
-    res.render('signup')
-}
+  res.render("sign-up");
+};
